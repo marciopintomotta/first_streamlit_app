5a21bc2e8c13486458ccdf48b642954b6a68a5b0
Marcio Pinto Araujo <marciopintomotta@gmail.com>
2023-07-07 09:42:10 +0100

    add request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 
 streamlit.title("My Mom's New Healthy Diner")
@@ -23,4 +24,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
