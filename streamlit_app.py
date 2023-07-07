f193bf941f0fe1136a41186739d3809ad6a1480f
Marcio Pinto Araujo <marciopintomotta@gmail.com>
2023-07-07 12:56:38 +0100

    avocado

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,5 +55,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 
-fruit_add = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', fruit_add)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into FRUIT_LOAD_LIST('from streamlit')")
